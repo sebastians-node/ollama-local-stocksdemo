59f932cc6c914932d8d54d0d093b4dc6eb04ddab
sebastians-node <sebastian.esco99@gmail.com>
2025-08-06 17:57:00 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+You are an exeprt in answering questions about stocks 
+
+Here are some relevant company: {company}
+
+Here is the question to answer: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+while True:
+    print("\n\n-------------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
+    if question == "q":
+        break
+    
+    company = retriever.invoke(question)
+    result = chain.invoke({"company": company, "question": question})
+    print(result)
